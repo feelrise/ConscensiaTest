a51d3e63b360c77aeaef57669c4871d070e4e75a
Ruslan Filonenko <filonenko_ruslan@outlook.com>
2020-03-31 21:50:27 +0300

    initial

---
+++ ConscensiaTest.Api/Controllers/ConscensiaController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ConscensiaTest.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class ConscensiaController : ControllerBase
+    {
+        private readonly ILogger<ConscensiaController> _logger;
+
+        public ConscensiaController(ILogger<ConscensiaController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public Int32 GetRandomNumber()
+        {
+            var rng = new Random();
+            return rng.Next(100);
+        }
+    }
+}
---
+++ ConscensiaTest.Client/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Configuration;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+using Newtonsoft.Json;
+
+namespace ConscensiaTest.Client
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly String BaseUrl = ConfigurationManager.AppSettings["baseUrl"];
+        private const String RandomNumberPath = "Conscensia/GetRandomNumber";
+        private readonly HttpClient _httpClient = new HttpClient();
+        private readonly Slider _slider;
+
+        public MainWindow()
+        {
+            var timer = new DispatcherTimer {Interval = TimeSpan.FromSeconds(1)};
+            InitializeComponent();
+
+            _slider = Slider;
+            timer.Tick += OnTick;
+            timer.Start();
+        }
+
+        private async void OnTick(Object sender, EventArgs args)
+        {
+            _slider.Value = await GetRandomNumberAsync();
+        }
+
+        private async Task<Int32> GetRandomNumberAsync()
+        {
+            using var response = await _httpClient.GetAsync(BuildUrl(RandomNumberPath));
+            var content = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<Int32>(content);
+        }
+
+        private static String BuildUrl(String path)
+        {
+            return BaseUrl + path;
+        }
+    }
+}
